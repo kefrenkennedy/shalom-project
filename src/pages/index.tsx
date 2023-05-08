7a0c5f15eecedab518ec47bf20337c7983e6fdef
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-08 20:14:15 -0300

    fix: move assets to public folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,34 @@
-import Header from '@/components/header';
+import React, { Component } from 'react';
 import {
-  Box,
   Button,
   FormControl,
-  FormErrorMessage,
   FormLabel,
-  Grid,
-  Heading,
   Image,
   Input,
-  VStack,
+  Select,
 } from '@chakra-ui/react';
-import AcampsBeach from '../assets/AcampsBeach.png';
-import AcampsBeachFooter from '../assets/AcampsBeachFooter.png';
-import carrossel1 from '../assets/Carrossel1.jpg';
-import carrossel2 from '../assets/Carrossel2.jpg';
-import carrossel3 from '../assets/Carrossel3.jpg';
-import carrossel4 from '../assets/Carrossel4.jpg';
-import carrossel5 from '../assets/Carrossel5.jpg';
-import Footer from '../assets/Footer.png';
-import FormularioDeInscrição from '../assets/FormularioDeInscrição.png';
-import Lotes from '../assets/Lotes.png';
-import React, { Component } from 'react';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+
+import Header from '@/components/Header';
+import AcampsBeach from '../../public/assets/AcampsBeach.png';
+import AcampsBeachFooter from '../../public/assets/AcampsBeachFooter.png';
+import carrossel1 from '../../public/assets/Carrossel1.jpg';
+import carrossel2 from '../../public/assets/Carrossel2.jpg';
+import carrossel3 from '../../public/assets/Carrossel3.jpg';
+import carrossel4 from '../../public/assets/Carrossel4.jpg';
+import carrossel5 from '../../public/assets/Carrossel5.jpg';
+import Footer from '../../public/assets/Footer.png';
+import FormularioDeInscrição from '../../public/assets/FormularioDeInscrição.png';
+import Lotes from '../../public/assets/Lotes.png';
 
 const schema = z.object({
   nomeCompleto: z.string().nonempty(),
   idade: z.number().int().min(1).max(150),
-  telefone: z.string().pattern(/^\d{10}$/),
+  telefone: z.string(),
   nomeResponsavel: z.string().optional(),
   numeroResponsavel: z.string().optional(),
   grupoOração: z.string().optional(),
@@ -66,18 +63,17 @@ class DemoCarousel extends Component {
   }
 }
 
-const { register, handleSubmit, formState } = useForm({
-  resolver: zodResolver(schema),
-  mode: 'onBlur',
-});
-
-const { errors } = formState;
-
-const onSubmit = (data: any) => {
-  console.log(data);
-};
-
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const { errors } = formState;
+
+  // const onSubmit: SubmitHandler<> = (data: any) => {
+  //   console.log(data);
+  // };
+
   return (
     <>
       <Header />
@@ -102,96 +98,63 @@ export default function Home() {
         ml="60px"
       />
 
-      <form onSubmit={handleSubmit(onsubmit)}>
-        <FormControl isInvalid={errors.nomeCompleto}>
+      <form onSubmit={() => {}}>
+        <FormControl>
           <FormLabel>NOME COMPLETO:</FormLabel>
           <Input {...register('nomeCompleto')} />
-          <FormErrorMessage>
-            {errors.nomeCompleto && errors.nomeCompleto.message}
-          </FormErrorMessage>
         </FormControl>
 
-        <FormControl isInvalid={errors.idade}>
+        <FormControl>
           <FormLabel>SUA IDADE:</FormLabel>
           <Input type="number" {...register('idade')} />
-          <FormErrorMessage>
-            {errors.idade && errors.idade.message}
-          </FormErrorMessage>
         </FormControl>
 
-        <FormControl isInvalid={errors.telefone}>
+        <FormControl>
           <FormLabel>TELEFONE PARA CONTATO:</FormLabel>
           <Input {...register('telefone')} />
-          <FormErrorMessage>
-            {errors.telefone && errors.telefone.message}
-          </FormErrorMessage>
         </FormControl>
 
-        <FormControl isInvalid={errors.nomeResponsavel}>
+        <FormControl>
           <FormLabel>SE MENOR DE IDADE, NOME DO RESPONSÁVEL:</FormLabel>
           <Input {...register('nomeResponsavel')} />
-          <FormErrorMessage>
-            {errors.nomeResponsavel && errors.nomeResponsavel.message}
-          </FormErrorMessage>
         </FormControl>
 
-        <FormControl isInvalid={errors.numeroResponsavel}>
+        <FormControl>
           <FormLabel>NÚMERO DO RESPONSÁVEL:</FormLabel>
           <Input {...register('numeroResponsavel')} />
-          <FormErrorMessage>
-            {errors.numeroResponsavel && errors.numeroResponsavel.message}
-          </FormErrorMessage>
         </FormControl>
 
         <FormControl>
           <FormLabel>VOCÊ PARTICIPA DA OBRA SHALOM:</FormLabel>
           <Input {...register('obraShalom')} />
-          <FormErrorMessage>
-            {errors.obraShalom && errors.obraShalom.message}
-          </FormErrorMessage>
         </FormControl>
 
-        <FormControl isInvalid={errors.grupoOração}>
+        <FormControl>
           <FormLabel>SE SIM, QUAL O NOME DO GRUPO DE ORAÇÃO?</FormLabel>
           <Input {...register('grupoOração')} />
-          <FormErrorMessage>
-            {errors.grupoOração && errors.grupoOração.message}
-          </FormErrorMessage>
         </FormControl>
 
-        <FormControl isInvalid={errors.membroComunidade}>
+        <FormControl>
           <FormLabel>VOCÊ É MEMBRO DA COMUNIDADE DE VIDA OU ALIANÇA?</FormLabel>
           <Input {...register('aliançaOuVida')} />
-          <FormErrorMessage>
-            {errors.aliançaOuVida && errors.aliançaOuVida.message}
-          </FormErrorMessage>
         </FormControl>
 
-        <FormControl isInvalid={errors.alergias}>
+        <FormControl>
           <FormLabel>VOCÊ É ALÉRGICO A ALGUMA COMIDA OU REMÉDIO?</FormLabel>
           <Input {...register('alergias')} />
-          <FormErrorMessage>
-            {errors.alergias && errors.alergias.message}
-          </FormErrorMessage>
         </FormControl>
 
-        <FormControl isInvalid={errors.email}>
+        <FormControl>
           <FormLabel>DIGITE SEU E-MAIL PARA LOGIN:</FormLabel>
           <Input type="email" {...register('email')} />
-          <FormErrorMessage>
-            {errors.email && errors.email.message}
-          </FormErrorMessage>
         </FormControl>
 
-        <FormControl isInvalid={errors.senha}>
+        <FormControl>
           <FormLabel>CADASTRE UMA SENHA DE 8 DÍGITOS</FormLabel>
           <Input type="password" {...register('senha')} />
-          <FormErrorMessage>
-            {errors.senha && errors.senha.message}
-          </FormErrorMessage>
         </FormControl>
 
-        <FormControl isInvalid={errors.formaPagamento}>
+        <FormControl>
           <FormLabel>SELECIONE A FORMA DE PAGAMENTO</FormLabel>
           <Select {...register('formaPagamento')}>
             <option value="">Selecione uma opção</option>
@@ -201,9 +164,6 @@ export default function Home() {
               Cartão de Crédito/Débito
             </option>
           </Select>
-          <FormErrorMessage>
-            {errors.formaPagamento && errors.formaPagamento.message}
-          </FormErrorMessage>
         </FormControl>
 
         <Button mt={4} colorScheme="teal" type="submit">
