4f82bd3c6285920922b2f8f011c41d909ce80da8
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-09 00:42:14 -0300

    feat: add AppProvider to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+
 import { theme } from '@/styles/theme';
+import { AppProvider } from '@/hooks';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AppProvider>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AppProvider>
   );
 }
