bfabc1c137551b7a5a7b1a5c3cb2f970dfb5ac50
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-09 09:29:28 -0300

    fix: ignore typing error on Inputmasked

--- src/components/forms/InputMasked/index.tsx
+++ src/components/forms/InputMasked/index.tsx
@@ -4,10 +4,10 @@ import {
   FormControl,
   InputProps as ChakraInputProps,
   FormErrorMessage,
-} from "@chakra-ui/react";
-import { ForwardRefRenderFunction, forwardRef } from "react";
-import { FieldError } from "react-hook-form";
-import InputMask from "react-input-mask";
+} from '@chakra-ui/react';
+import React, { ForwardRefRenderFunction, forwardRef } from 'react';
+import { FieldError } from 'react-hook-form';
+import InputMask from 'react-input-mask';
 
 interface InputMaskProps extends ChakraInputProps {
   name: string;
@@ -16,31 +16,39 @@ interface InputMaskProps extends ChakraInputProps {
   mask: string;
 }
 
-const InputMaskBase: ForwardRefRenderFunction<HTMLInputElement, InputMaskProps> = (
-  { name, label, error, mask, ...rest }: InputMaskProps,
+const InputMaskBase: ForwardRefRenderFunction<
+  HTMLInputElement,
+  InputMaskProps
+> = (
+  { name, label, error, mask, onChange, onBlur, ...rest }: InputMaskProps,
   ref
 ) => {
+  const renderInput = () => (
+    <ChakraInput
+      {...rest}
+      id={name}
+      name={name}
+      borderColor="gray.900"
+      borderRadius="full"
+      focusBorderColor="green.200"
+      bgColor="gray.50"
+      variant="filled"
+      _hover={{
+        bgColor: 'gray.50',
+      }}
+      size="lg"
+      ref={ref}
+    />
+  );
+
   return (
     <FormControl isInvalid={!!error}>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-      <InputMask mask={mask} {...rest}>
-        {(inputProps) => (
-          <ChakraInput
-            {...inputProps}
-            id={name}
-            name={name}
-            borderColor="gray.900"
-            borderRadius="full"
-            focusBorderColor="green.200"
-            bgColor="gray.50"
-            variant="filled"
-            _hover={{
-              bgColor: "gray.50",
-            }}
-            size="lg"
-            ref={ref}
-          />
-        )}
+      <InputMask mask={mask}>
+        {
+          //@ts-ignore
+          renderInput
+        }
       </InputMask>
       {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
