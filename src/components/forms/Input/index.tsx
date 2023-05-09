986f30c24f088cd5b1f02aff6a970ee70216d1b2
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-09 00:35:20 -0300

    feat: create input form component

---
+++ src/components/forms/Input/index.tsx
@@ -0,0 +1,44 @@
+import {
+  Input as ChakraInput,
+  FormLabel,
+  FormControl,
+  InputProps as ChakraInputProps,
+  FormErrorMessage,
+} from '@chakra-ui/react';
+import { ForwardRefRenderFunction, RefAttributes, forwardRef } from 'react';
+import { FieldError } from 'react-hook-form';
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+  error?: FieldError;
+}
+
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error, ...rest }: InputProps,
+  ref
+) => {
+  return (
+    <FormControl isInvalid={!!error}>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        id={name}
+        name={name}
+        borderColor="gray.900"
+        borderRadius="full"
+        focusBorderColor="green.200"
+        bgColor="gray.50"
+        variant="filled"
+        _hover={{
+          bgColor: 'gray.50',
+        }}
+        size="lg"
+        ref={ref}
+        {...rest}
+      />
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+    </FormControl>
+  );
+};
+
+export const Input = forwardRef(InputBase);
