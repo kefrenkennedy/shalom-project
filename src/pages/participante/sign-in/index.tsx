4fdc13ff2731ec92f952f6ad25a5a563e81d3e5e
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-14 12:46:50 -0300

    feat: create participant registration and signin page

---
+++ src/pages/participante/sign-in/index.tsx
@@ -0,0 +1,101 @@
+import { useEffect } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+import { Button, Flex, Stack, Text } from '@chakra-ui/react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useRouter } from 'next/router';
+import * as yup from 'yup';
+
+import { Sidebar } from '@/components/Sidebar';
+import { useAuth } from '@/hooks/auth';
+import { withSSRGuest } from '@/utils/withSSRGuest';
+
+import { Input } from '../../../components/forms/Input';
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().email('E-mail inválido').required('E-mail obrigatório'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
+export default function SignIn() {
+  const { user, signIn } = useAuth();
+  const router = useRouter();
+
+  const { register, handleSubmit, formState } = useForm<SignInFormData>({
+    resolver: yupResolver(signInFormSchema),
+  });
+  const { errors } = formState;
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (data) => {
+    const { email, password } = data;
+    signIn({ email, password });
+  };
+
+  useEffect(() => {
+    if (user.id) {
+      router.push('/participante/inscricoes');
+    }
+  }, [user, router]);
+
+  return (
+    <Flex bg="gray.50" w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="white"
+        p="8"
+        borderRadius={20}
+        flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
+      >
+        <Stack spacing="4">
+          <Text fontWeight="bolder" align="center">
+            Portal do Participante
+          </Text>
+
+          <Input
+            type="email"
+            label="E-mail"
+            borderColor="gray.200"
+            bg="white"
+            {...register('email')}
+            error={errors.email}
+          />
+          <Input
+            type="password"
+            label="Senha"
+            {...register('password')}
+            borderColor="gray.200"
+            bg="white"
+            error={errors.password}
+          />
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          bg="green.200"
+          borderRadius="full"
+          colorScheme="green"
+          color="white"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
+}
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {},
+  };
+});
