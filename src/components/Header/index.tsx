a9686bd6780ddfd775077eda48d76fadb72c23c0
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-16 00:10:12 -0300

    style: adjust header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,56 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { useMemo } from 'react';
+
+import { Button, Flex, Text } from '@chakra-ui/react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+
+import { useAuth } from '@/hooks/auth';
 
 export function Header() {
+  const router = useRouter();
+  const { user } = useAuth();
+
+  const isUserLogged = useMemo(() => {
+    return !!user.id;
+  }, [user]);
+
+  function handleActionButton() {
+    router.push('/participante/sign-in');
+  }
+
   return (
     <>
       <Flex
-        textDecoration="underline"
         w="100%"
         display="flex"
         justifyContent="space-around"
         alignItems="center"
         bgColor="orange.500"
         color="white"
-        h="70px"
+        h="6rem"
+        px="1rem"
       >
-        <Box>{"Inscrição ACAMP'S BEACH"}</Box>
+        <Flex width={['20', '35', '40']} height={['30', '25', '20']}>
+          <Flex width="100%" height="100%" position="relative">
+            <Image
+              src="/assets/icone1.png"
+              alt="Logo Shalom"
+              fill
+              objectFit="cover"
+            />
+          </Flex>
+        </Flex>
+        <Text fontWeight="bold" fontSize={['md', '3xl']} textAlign="center">
+          {"ACAMP'S BEACH"}
+        </Text>
+        <Button
+          colorScheme="whiteAlpha"
+          borderRadius="full"
+          size={['md', 'lg']}
+          onClick={handleActionButton}
+        >
+          {isUserLogged ? 'Minhas inscrições' : 'Login'}
+        </Button>
         {/* <Box cursor="pointer">Sobre o Evento</Box> */}
         {/* <Box cursor="pointer">Contatos</Box> */}
       </Flex>
--- src/components/UserHeader/Logo.tsx
+++ src/components/UserHeader/Logo.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 
 import { useAuth } from '@/hooks/auth';
 
@@ -7,16 +7,28 @@ export function Logo() {
   const isAdmin = user.role === 'ADMINISTRATOR';
 
   return (
-    <Text
-      fontSize={isAdmin ? ['md', 'xl'] : ['2xl', '3xl']}
-      fontWeight="bold"
-      letterSpacing="tight"
-      w="64"
-    >
-      {isAdmin ? 'Administrativo ' : ''}Eventos SH
-      <Text as="span" ml="1" color="yellow.400">
-        .
+    <Flex direction="column">
+      {isAdmin && (
+        <Text
+          fontSize={isAdmin ? ['md', 'xl'] : ['2xl', '3xl']}
+          fontWeight="bold"
+          letterSpacing="tight"
+          w={['30', '64']}
+        >
+          Administrativo
+        </Text>
+      )}
+      <Text
+        fontSize={isAdmin ? ['md', 'xl'] : ['2xl', '3xl']}
+        fontWeight="bold"
+        letterSpacing="tight"
+        w={['30', '64']}
+      >
+        Eventos SH
+        <Text as="span" ml="1" color="yellow.400">
+          .
+        </Text>
       </Text>
-    </Text>
+    </Flex>
   );
 }
