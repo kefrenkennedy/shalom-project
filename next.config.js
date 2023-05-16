0e73ed5dab5eaea0bf26bae9445f95598440890d
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-15 23:14:50 -0300

    chore: add image domains to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'shalomsobral.com', 'api.shalomsobral.com'],
+  },
 };
 
 module.exports = nextConfig;
