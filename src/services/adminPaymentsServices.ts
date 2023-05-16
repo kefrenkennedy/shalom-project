195f3d3f0497be8934ae7465b8cd4fed792cf95c
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-15 23:05:04 -0300

    feat: create servive to admin updated payment

---
+++ src/services/adminPaymentsServices.ts
@@ -0,0 +1,10 @@
+import { api } from './apiClient';
+
+const PATH = '/payments';
+
+export const adminPaymentsServices = () => ({
+  update: async (payment_id: string) => {
+    const response = await api.patch(PATH + `${payment_id}/update-status`);
+    return response.data;
+  },
+});
