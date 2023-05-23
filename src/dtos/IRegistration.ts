f9d6be124afb8efd583912b7b5c5f22fa088587f
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-23 02:28:45 -0300

    refactor: refactor IRegistration

--- src/dtos/IRegistration.ts
+++ src/dtos/IRegistration.ts
@@ -1,27 +1,20 @@
 import { IEvent } from './IEvent';
+import { IParticipant } from './IParticipant';
 import { IPayment } from './IPayment';
 
 export interface IRegistration {
   id: string;
   user_id: string;
   event_id: string;
-  full_name: string;
-  phone_number: string;
-  age: number;
-  document_number: string;
-  document_type: string;
-  guardian_name?: string;
-  guardian_phone_number?: string;
-  prayer_group?: string;
+  credential_name: string;
   event_source?: string;
-  community_type?: string;
-  pcd_description?: string;
-  allergy_description?: string;
   transportation_mode: string;
   accepted_the_terms: boolean;
   is_approved: boolean;
-  created_at: string;
-  updated_at: string;
   event?: IEvent;
   payment?: IPayment;
+  user?: {
+    email: string;
+    participant: IParticipant;
+  };
 }
