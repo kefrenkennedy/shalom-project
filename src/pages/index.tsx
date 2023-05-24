2a06ed184328a506b00c712a77cfbef9fc054c10
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-23 22:31:26 -0300

    fix: hide registration form when admin is logged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 
-import { Flex, Image } from '@chakra-ui/react';
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
 
 import { Carousel } from '@/components/Carousel';
 import { Footer } from '@/components/Footer';
 import { RegistrationForm } from '@/components/forms/composites/RegistrationForm';
 import { Header } from '@/components/Header';
+import { useAuth } from '@/hooks/auth';
 
 import acampsBeachImage from '../../public/assets/AcampsBeach.png';
 import acampsBeachFooterImage from '../../public/assets/AcampsBeachFooter.png';
 import lotesImage from '../../public/assets/Lotes.png';
 
 export default function Home() {
+  const { user } = useAuth();
+
   return (
     <Flex direction="column">
       <Header />
@@ -36,7 +39,23 @@ export default function Home() {
 
       <Image src={lotesImage.src} alt="Acamps Beach Lotes" w="100%" />
 
-      <RegistrationForm />
+      {user.role === 'ADMINISTRATOR' ? (
+        <Flex align="center" justify="center" p="4rem">
+          <Box w={900}>
+            <Text
+              fontSize="3xl"
+              textAlign="center"
+              fontWeight="medium"
+              color="gray.500"
+            >
+              Você está loggado como administrador, mas apenas participantes
+              podem realizar a inscrição no evento
+            </Text>
+          </Box>
+        </Flex>
+      ) : (
+        <RegistrationForm />
+      )}
 
       <Footer />
     </Flex>
