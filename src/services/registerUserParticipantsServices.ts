6c7f411e97dff745f66440f89af733206c21b57d
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-10-31 22:09:40 -0300

    refactor: accept form data on registerUserParticipantsServices

--- src/services/registerUserParticipantsServices.ts
+++ src/services/registerUserParticipantsServices.ts
@@ -2,36 +2,8 @@ import { api } from './apiClient';
 
 const PATH = '/participants/register';
 
-interface ICreateRequest {
-  name: string;
-  email: string;
-  password: string;
-  password_confirmation: string;
-
-  street: string;
-  street_number: string;
-  complement?: string;
-  zip_code: string;
-  district: string;
-  city: string;
-  state: string;
-
-  full_name: string;
-  phone_number: string;
-  birthdate: Date;
-  document_number: string;
-  document_type: string;
-  guardian_name?: string;
-  guardian_phone_number?: string;
-  prayer_group?: string;
-  community_type?: string;
-  pcd_description?: string;
-  allergy_description?: string;
-  medication_use_description?: string;
-}
-
 export const registerUserParticipantsServices = () => ({
-  create: async (data: ICreateRequest) => {
+  create: async (data: FormData) => {
     const response = await api.post(PATH, data);
     return response.data;
   },
